94a25ff20cff065901b5a5836931a52377e24cde
Marcell Schuh <marcell.schuh@gmail.com>
2021-04-26 13:54:38 +0200

    Add proper dvd logo path

--- DVDLogo.cpp
+++ DVDLogo.cpp
@@ -1,19 +1,21 @@
 #include "DVDLogo.h"
 
-int16_t DVDLogo::calcX(float time) {
-    float mult = (sin(time * (2 * M_PI / 10.f)) + 1) / 2.f;
-    return (int16_t) (mult * 96);
-}
-
-int16_t DVDLogo::calcY(float time) {
-    float mult = (sin(time * (2 * M_PI / 2.f)) + 1) / 2.f;
-    return (int16_t) (mult * 48);
+void DVDLogo::draw() {
+    display.clearDisplay();
+    display.drawBitmap(currentX, currentY, dvdLogo, 32, 16, WHITE);
+    display.display();
 }
 
 void DVDLogo::render(u_long delta) {
-    totalTime += delta;
-    display.clearDisplay();
-    display.drawBitmap(calcX(totalTime / 1000.f), calcY(totalTime / 1000.f), dvdLogo, 32, 16, WHITE);
-    display.display();
-
+    elapsedTime += delta;
+    if(elapsedTime > FRAME_TIME) {
+        currentX += velocityX;
+        currentY += velocityY;
+        if(currentX + LOGO_WIDTH == SCREEN_WIDTH) velocityX = -velocityX;
+        if(currentY + LOGO_HEIGHT == SCREEN_HEIGHT) velocityY = -velocityY;
+        if(currentX == 0) velocityX = -velocityX;
+        if(currentY == 0) velocityY = -velocityY;
+        draw();
+        elapsedTime = 0;
+    }
 }
--- DVDLogo.h
+++ DVDLogo.h
@@ -6,17 +6,26 @@
 #include <Adafruit_SSD1306.h>
 #include "bitmaps.h"
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
 
 class DVDLogo {
     public:
-        DVDLogo(Adafruit_SSD1306 &display): display(display), totalTime(0), prevSec(0) {};
+        DVDLogo(Adafruit_SSD1306 &display): display(display), elapsedTime(0), currentX(0), currentY(10), velocityX(1), velocityY(1) {};
         void render(u_long delta);
     private:
         Adafruit_SSD1306 &display;
-        int16_t calcX(float time);
-        int16_t calcY(float time);
-        u_long totalTime;
-        u_long prevSec;
+        const int16_t LOGO_WIDTH = 32;
+        const int16_t LOGO_HEIGHT = 16;
+        const float FRAME_TIME = 1.f / 60.f; // 60 fps
+        float elapsedTime;
+        int16_t currentX;
+        int16_t currentY;
+        int16_t velocityX;
+        int16_t velocityY;
+
+        void draw();
 };
 
 
